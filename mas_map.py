6a65eb65e1cbcede4dc78e2b4fe70d1240b29e9d
LimJungWoo <presidentljw@naver.com>
2025-07-21 14:57:24 +0900

    chore(1단계): mas_map.py 스크립트 수정

--- mas_map.py
+++ mas_map.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
+import sys
+output_file = None
+area_number = None
+if len(sys.argv) == 1:
+    output_file = 'all_area.csv'
+elif len(sys.argv) == 2:
+    try:
+        area_number = int(sys.argv[1])
+    except ValueError:
+        print("Please provide a valid area number.")
+        sys.exit(1)
+    output_file = f'area{sys.argv[1]}.csv'
+else:
+    print("Usage: python mas_map.py [area_number]")
+    sys.exit(1)
+
 area_map = pd.read_csv('area_map.csv')
 area_struct = pd.read_csv('area_struct.csv')
 area_category = pd.read_csv('area_category.csv')
@@ -14,16 +30,17 @@ merged = pd.merge(area_map, struct_with_category, on = ['x', 'y'], how = 'left')
 
 merged_sorted = merged.sort_values(by = ['area', 'x', 'y'])
 # print(merged_sorted.head(30))
-area1 = merged_sorted[merged_sorted['area'] == 1]
+if area_number is not None:
+    merged_sorted = merged_sorted[merged_sorted['area'] == area_number]
 
-cols = ['area'] + [col for col in area1.columns if col != 'area']
+cols = ['area'] + [col for col in merged_sorted.columns if col != 'area']
 cols = [col for col in cols if col != 'ConstructionSite'] + ['ConstructionSite']
 # print(cols)
-area1 = area1[cols]
-# print(area1.head(30))
-# print(area1.columns)
+merged_sorted = merged_sorted[cols]
+# print(merged_sorted.head(30))
+# print(merged_sorted.columns)
 
-area1.fillna('없음').to_csv('area1.csv', index = False)
+merged_sorted.fillna('없음').to_csv(f'{output_file}', index = False)
 
 report = merged_sorted.groupby('struct').agg(
     count = ('struct', 'count'),
