7e16ca467fc8b8b63dca8fea3c4718272a2436f2
LimJungWoo <presidentljw@naver.com>
2025-07-21 14:31:55 +0900

    chore(1단계): mas_map.py 수정

--- mas_map.py
+++ mas_map.py
@@ -6,12 +6,13 @@ area_category = pd.read_csv('area_category.csv')
 area_category.columns = area_category.columns.str.strip()
 
 struct_with_category = pd.merge(area_struct, area_category, on = 'category', how = 'left')
+struct_with_category.drop(columns = ['category'], inplace = True)
 # print(struct_with_category.head(30))
 
 merged = pd.merge(area_map, struct_with_category, on = ['x', 'y'], how = 'left')
 # print(merged.head(30))
 
-merged_sorted = merged.sort_values(by = 'area')
+merged_sorted = merged.sort_values(by = ['area', 'x', 'y'])
 # print(merged_sorted.head(30))
 area1 = merged_sorted[merged_sorted['area'] == 1]
 
@@ -20,7 +21,9 @@ cols = [col for col in cols if col != 'ConstructionSite'] + ['ConstructionSite']
 # print(cols)
 area1 = area1[cols]
 # print(area1.head(30))
-print(area1.columns)
+# print(area1.columns)
+
+area1.fillna('없음').to_csv('area1.csv', index = False)
 
 report = merged_sorted.groupby('struct').agg(
     count = ('struct', 'count'),
